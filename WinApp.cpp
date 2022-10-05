7ba03b21e331a3d061a7cd6492d1b866d27f7bc6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-05 11:08:49 +0900

    初期化処理云々をmain.cppから引っ越し。

--- WinApp.cpp
+++ WinApp.cpp
@@ -2,6 +2,45 @@
 
 void WinApp::Initialize()
 {
+	//サイズ
+	const int window_width = 1280;
+	const int window_height = 720;
+
+	const float PI = 3.1415926535f;
+
+
+
+	//クラス設定
+	WNDCLASSEX w{};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProc;
+	w.lpszClassName = L"DirectXGame";
+	w.hInstance = GetModuleHandle(nullptr);
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+	//OSに登録
+	RegisterClassEx(&w);
+	//サイズ
+	RECT wrc = { 0,0,window_width,window_height };
+	//自動でサイズ修正
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(w.lpszClassName,
+		L"LE2B_01_アカイケ_カケル_CG2",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		w.hInstance,
+		nullptr);
+
+	//表示状態にする
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};//メッセージ
 }
 
 void WinApp::Update()
--- main.cpp
+++ main.cpp
@@ -290,45 +290,45 @@ LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//------WindowsAPI初期化処理 ここから------
-	//サイズ
-	const int window_width = 1280;
-	const int window_height = 720;
+	////サイズ
+	//const int window_width = 1280;
+	//const int window_height = 720;
 
-	const float PI = 3.1415926535f;
+	//const float PI = 3.1415926535f;
 
 
 
-	//クラス設定
-	WNDCLASSEX w{};
-	w.cbSize = sizeof(WNDCLASSEX);
-	w.lpfnWndProc = (WNDPROC)WindowProc;
-	w.lpszClassName = L"DirectXGame";
-	w.hInstance = GetModuleHandle(nullptr);
-	w.hCursor = LoadCursor(NULL, IDC_ARROW);
+	////クラス設定
+	//WNDCLASSEX w{};
+	//w.cbSize = sizeof(WNDCLASSEX);
+	//w.lpfnWndProc = (WNDPROC)WindowProc;
+	//w.lpszClassName = L"DirectXGame";
+	//w.hInstance = GetModuleHandle(nullptr);
+	//w.hCursor = LoadCursor(NULL, IDC_ARROW);
 
-	//OSに登録
-	RegisterClassEx(&w);
-	//サイズ
-	RECT wrc = { 0,0,window_width,window_height };
-	//自動でサイズ修正
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	////OSに登録
+	//RegisterClassEx(&w);
+	////サイズ
+	//RECT wrc = { 0,0,window_width,window_height };
+	////自動でサイズ修正
+	//AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-	HWND hwnd = CreateWindow(w.lpszClassName,
-		L"LE2B_01_アカイケ_カケル_CG2",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		wrc.right - wrc.left,
-		wrc.bottom - wrc.top,
-		nullptr,
-		nullptr,
-		w.hInstance,
-		nullptr);
+	//HWND hwnd = CreateWindow(w.lpszClassName,
+	//	L"LE2B_01_アカイケ_カケル_CG2",
+	//	WS_OVERLAPPEDWINDOW,
+	//	CW_USEDEFAULT,
+	//	CW_USEDEFAULT,
+	//	wrc.right - wrc.left,
+	//	wrc.bottom - wrc.top,
+	//	nullptr,
+	//	nullptr,
+	//	w.hInstance,
+	//	nullptr);
 
-	//表示状態にする
-	ShowWindow(hwnd, SW_SHOW);
+	////表示状態にする
+	//ShowWindow(hwnd, SW_SHOW);
 
-	MSG msg{};//メッセージ
+	//MSG msg{};//メッセージ
 			  //------WindowsAPI初期化処理 ここまで------
 
 			  //------DirectX初期化処理 ここから------
