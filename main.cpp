d96d81b263629e3cd1e08277ff0313c3e3c39551
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-11 11:01:25 +0900

    とりあえずグラフィックスパイプライン関連の設定処理を、main.cppから引っ越し。

--- main.cpp
+++ main.cpp
@@ -540,80 +540,80 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		//{/*...*/},
 	};
 
-	//グラフィックスパイプライン設定
-	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
-
-	//シェーダーの設定
-	pipelineDesc.VS.pShaderBytecode = vsBlob->GetBufferPointer();
-	pipelineDesc.VS.BytecodeLength = vsBlob->GetBufferSize();
-	pipelineDesc.PS.pShaderBytecode = psBlob->GetBufferPointer();
-	pipelineDesc.PS.BytecodeLength = psBlob->GetBufferSize();
-
-	//サンプルマスクの設定
-	pipelineDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;//標準設定
-	//pipelineDesc.SampleMask = UINT_MAX;
-
-	//ラスタライザの設定
-	//背面カリングも設定する
-	//pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;//カリングしない
-	pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;//背面カリングする
-	pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;//ポリゴン内塗りつぶし
-	pipelineDesc.RasterizerState.DepthClipEnable = true;//深度クリッピングを有効に
-
-	//ブレンドステート
-	pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask 
-		= D3D12_COLOR_WRITE_ENABLE_ALL;//RGB全てのチャネルを描画
-
-	//レンダ―ターゲットのブレンド設定
-	D3D12_RENDER_TARGET_BLEND_DESC& blenddesc = pipelineDesc.BlendState.RenderTarget[0];
-	blenddesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
-
-	//アルファ値共通設定
-	blenddesc.BlendEnable = false; // ブレンド有効にする
-	blenddesc.BlendOpAlpha = D3D12_BLEND_OP_ADD; //ブレンドを有効にする
-	blenddesc.SrcBlendAlpha = D3D12_BLEND_ONE; //加算
-	blenddesc.DestBlendAlpha = D3D12_BLEND_ZERO; //デストの値を 0%使う　
-
-	//加算合成
-	//blenddesc.BlendOp = D3D12_BLEND_OP_ADD; //加算
-	//blenddesc.SrcBlend = D3D12_BLEND_ONE; //ソースの値を100%使う
-	//blenddesc.DestBlend = D3D12_BLEND_ONE; //デストの値を100%使う
-
-	//減算合成
-	//blenddesc.BlendOp = D3D12_BLEND_OP_REV_SUBTRACT; //減算
-	//blenddesc.SrcBlend = D3D12_BLEND_ONE; //ソースの値を100%使う
-	//blenddesc.DestBlend = D3D12_BLEND_ONE; //デストの値を100%使う
-
-	//色反転
-	//blenddesc.BlendOp = D3D12_BLEND_OP_ADD; //加算
-	//blenddesc.SrcBlend = D3D12_BLEND_INV_DEST_COLOR; //1.0f-デストから－の値
-	//blenddesc.DestBlend = D3D12_BLEND_ZERO; //使わない
-
-	//半透明合成
-	blenddesc.BlendOp = D3D12_BLEND_OP_ADD; //加算
-	blenddesc.SrcBlend = D3D12_BLEND_ONE; //ソースの値をアルファ値
-	blenddesc.DestBlend = D3D12_BLEND_ONE; //1.0f-ソースのアルファ値
-
-	//頂点レイアウトの設定
-	pipelineDesc.InputLayout.pInputElementDescs = inputLayout;
-	pipelineDesc.InputLayout.NumElements = _countof(inputLayout);
-
-	//図形の形状設定
-	pipelineDesc.PrimitiveTopologyType
-		= D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-
-#pragma region 深度テストの設定
-	//デプスステンシルステートの設定
-	pipelineDesc.DepthStencilState.DepthEnable = true;
-	pipelineDesc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
-	pipelineDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
-	pipelineDesc.DSVFormat = DXGI_FORMAT_R32_FLOAT;
-#pragma endregion
-
-	//その他の設定
-	pipelineDesc.NumRenderTargets = 1;//描画対象は1つ
-	pipelineDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;//0～255指定のRGBA
-	pipelineDesc.SampleDesc.Count = 1;//1ピクセルにつき1回サンプリング
+//	//グラフィックスパイプライン設定
+//	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
+//
+//	//シェーダーの設定
+//	pipelineDesc.VS.pShaderBytecode = vsBlob->GetBufferPointer();
+//	pipelineDesc.VS.BytecodeLength = vsBlob->GetBufferSize();
+//	pipelineDesc.PS.pShaderBytecode = psBlob->GetBufferPointer();
+//	pipelineDesc.PS.BytecodeLength = psBlob->GetBufferSize();
+//
+//	//サンプルマスクの設定
+//	pipelineDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;//標準設定
+//	//pipelineDesc.SampleMask = UINT_MAX;
+//
+//	//ラスタライザの設定
+//	//背面カリングも設定する
+//	//pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;//カリングしない
+//	pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;//背面カリングする
+//	pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;//ポリゴン内塗りつぶし
+//	pipelineDesc.RasterizerState.DepthClipEnable = true;//深度クリッピングを有効に
+//
+//	//ブレンドステート
+//	pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask 
+//		= D3D12_COLOR_WRITE_ENABLE_ALL;//RGB全てのチャネルを描画
+//
+//	//レンダ―ターゲットのブレンド設定
+//	D3D12_RENDER_TARGET_BLEND_DESC& blenddesc = pipelineDesc.BlendState.RenderTarget[0];
+//	blenddesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+//
+//	//アルファ値共通設定
+//	blenddesc.BlendEnable = false; // ブレンド有効にする
+//	blenddesc.BlendOpAlpha = D3D12_BLEND_OP_ADD; //ブレンドを有効にする
+//	blenddesc.SrcBlendAlpha = D3D12_BLEND_ONE; //加算
+//	blenddesc.DestBlendAlpha = D3D12_BLEND_ZERO; //デストの値を 0%使う　
+//
+//	//加算合成
+//	//blenddesc.BlendOp = D3D12_BLEND_OP_ADD; //加算
+//	//blenddesc.SrcBlend = D3D12_BLEND_ONE; //ソースの値を100%使う
+//	//blenddesc.DestBlend = D3D12_BLEND_ONE; //デストの値を100%使う
+//
+//	//減算合成
+//	//blenddesc.BlendOp = D3D12_BLEND_OP_REV_SUBTRACT; //減算
+//	//blenddesc.SrcBlend = D3D12_BLEND_ONE; //ソースの値を100%使う
+//	//blenddesc.DestBlend = D3D12_BLEND_ONE; //デストの値を100%使う
+//
+//	//色反転
+//	//blenddesc.BlendOp = D3D12_BLEND_OP_ADD; //加算
+//	//blenddesc.SrcBlend = D3D12_BLEND_INV_DEST_COLOR; //1.0f-デストから－の値
+//	//blenddesc.DestBlend = D3D12_BLEND_ZERO; //使わない
+//
+//	//半透明合成
+//	blenddesc.BlendOp = D3D12_BLEND_OP_ADD; //加算
+//	blenddesc.SrcBlend = D3D12_BLEND_ONE; //ソースの値をアルファ値
+//	blenddesc.DestBlend = D3D12_BLEND_ONE; //1.0f-ソースのアルファ値
+//
+//	//頂点レイアウトの設定
+//	pipelineDesc.InputLayout.pInputElementDescs = inputLayout;
+//	pipelineDesc.InputLayout.NumElements = _countof(inputLayout);
+//
+//	//図形の形状設定
+//	pipelineDesc.PrimitiveTopologyType
+//		= D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+//
+//#pragma region 深度テストの設定
+//	//デプスステンシルステートの設定
+//	pipelineDesc.DepthStencilState.DepthEnable = true;
+//	pipelineDesc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+//	pipelineDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
+//	pipelineDesc.DSVFormat = DXGI_FORMAT_R32_FLOAT;
+//#pragma endregion
+//
+//	//その他の設定
+//	pipelineDesc.NumRenderTargets = 1;//描画対象は1つ
+//	pipelineDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;//0～255指定のRGBA
+//	pipelineDesc.SampleDesc.Count = 1;//1ピクセルにつき1回サンプリング
 
 	//ルートシグネチャ
 	ComPtr<ID3D12RootSignature> rootSignature;
