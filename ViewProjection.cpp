0f832cc8ee34889ac2277e0c2523e6f9783eedca
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-18 10:16:43 +0900

    cppからヘッダーに、メンバ変数にするためにいくつかを引っ越し。

--- ViewProjection.cpp
+++ ViewProjection.cpp
@@ -1,12 +1,16 @@
 #include "ViewProjection.h"
-#include <DirectXMath.h>
 #include "WinApp.h"
-using namespace DirectX;
 
 void ViewProjection::Initialize() {
+
+	eye_ = { 0, 0, -100 };	//視点座標
+	target_ = { 0, 0, 0 };	//注視点座標
+	up_={0, 1, 0};		//上方向ベクトル
+
+
 #pragma region 投資投影変換行列の計算
 
-	XMMATRIX matProjection =
+	matProjection_ =
 		XMMatrixPerspectiveFovLH(
 			XMConvertToRadians(45.0f),//上下画角45度
 			(float)WinApp::WinWidth / WinApp::WinHeight,//アスペクト比(画面横幅/画面縦幅)
@@ -14,12 +18,9 @@ void ViewProjection::Initialize() {
 		);//前端、奥端
 
 #pragma region ビュー行列の作成
-	XMMATRIX matView;
-	XMFLOAT3 eye(0, 0, -100);	//視点座標
-	XMFLOAT3 target(0, 0, 0);	//注視点座標
-	XMFLOAT3 up(0, 1, 0);		//上方向ベクトル
-	matView = XMMatrixLookAtLH(XMLoadFloat3(&eye),
-		XMLoadFloat3(&target), XMLoadFloat3(&up));
+
+	matView_ = XMMatrixLookAtLH(XMLoadFloat3(&eye_),
+		XMLoadFloat3(&target_), XMLoadFloat3(&up_));
 
 #pragma endregion
 
--- ViewProjection.h
+++ ViewProjection.h
@@ -1,6 +1,18 @@
 #pragma once
+#include <DirectXMath.h>
+
+using namespace DirectX;
 
 class ViewProjection {
+
 public:
 	void Initialize();
+public:
+	XMMATRIX matProjection_; //プロジェクション行列
+
+	XMMATRIX matView_; //ビュー行列
+
+	XMFLOAT3 eye_;	//視点座標
+	XMFLOAT3 target_;	//注視点座標
+	XMFLOAT3 up_;		//上方向ベクトル
 };
