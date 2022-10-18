ffd9ac89bb025854b974065308a9caed28a7db77
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-18 09:59:39 +0900

    とりあえず処理をお引越し。

--- ViewProjection.cpp
+++ ViewProjection.cpp
@@ -1,4 +1,24 @@
 #include "ViewProjection.h"
 
 void ViewProjection::Initialize() {
+#pragma region 投資投影変換行列の計算
+
+	XMMATRIX matProjection =
+		XMMatrixPerspectiveFovLH(
+			XMConvertToRadians(45.0f),//上下画角45度
+			(float)WinApp::WinWidth / WinApp::WinHeight,//アスペクト比(画面横幅/画面縦幅)
+			0.1f, 1000.0f
+		);//前端、奥端
+
+#pragma region ビュー行列の作成
+	XMMATRIX matView;
+	XMFLOAT3 eye(0, 0, -100);	//視点座標
+	XMFLOAT3 target(0, 0, 0);	//注視点座標
+	XMFLOAT3 up(0, 1, 0);		//上方向ベクトル
+	matView = XMMatrixLookAtLH(XMLoadFloat3(&eye),
+		XMLoadFloat3(&target), XMLoadFloat3(&up));
+
+#pragma endregion
+
+#pragma endregion
 }
--- main.cpp
+++ main.cpp
@@ -99,26 +99,26 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//	0.0f, 1.0f);//前端、奥端
 #pragma endregion
 
-#pragma region 投資投影変換行列の計算
-
-	XMMATRIX matProjection =
-		XMMatrixPerspectiveFovLH(
-			XMConvertToRadians(45.0f),//上下画角45度
-			(float)WinApp::WinWidth / WinApp::WinHeight,//アスペクト比(画面横幅/画面縦幅)
-			0.1f, 1000.0f
-		);//前端、奥端
-
-#pragma region ビュー行列の作成
-	XMMATRIX matView;
-	XMFLOAT3 eye(0, 0, -100);	//視点座標
-	XMFLOAT3 target(0, 0, 0);	//注視点座標
-	XMFLOAT3 up(0, 1, 0);		//上方向ベクトル
-	matView = XMMatrixLookAtLH(XMLoadFloat3(&eye),
-		XMLoadFloat3(&target), XMLoadFloat3(&up));
-
-#pragma endregion
-
-#pragma endregion
+//#pragma region 投資投影変換行列の計算
+//
+//	XMMATRIX matProjection =
+//		XMMatrixPerspectiveFovLH(
+//			XMConvertToRadians(45.0f),//上下画角45度
+//			(float)WinApp::WinWidth / WinApp::WinHeight,//アスペクト比(画面横幅/画面縦幅)
+//			0.1f, 1000.0f
+//		);//前端、奥端
+//
+//#pragma region ビュー行列の作成
+//	XMMATRIX matView;
+//	XMFLOAT3 eye(0, 0, -100);	//視点座標
+//	XMFLOAT3 target(0, 0, 0);	//注視点座標
+//	XMFLOAT3 up(0, 1, 0);		//上方向ベクトル
+//	matView = XMMatrixLookAtLH(XMLoadFloat3(&eye),
+//		XMLoadFloat3(&target), XMLoadFloat3(&up));
+//
+//#pragma endregion
+//
+//#pragma endregion
 
 	//------描画初期化処理 ここまで------
 	//ゲームループ
