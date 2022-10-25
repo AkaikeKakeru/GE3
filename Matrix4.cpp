fcd1631e2d88dc4963e135ae64be59f4a5c2aa3d
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-25 20:56:59 +0900

    Matrix4 *= Matrix4 の戻り値を間違えていたので修正+改行と空白の調整

--- Matrix4.cpp
+++ Matrix4.cpp
@@ -3,7 +3,7 @@
 #include <cmath> //sin cos
 
 // 単位行列を求める
-Matrix4 MatIdentity(){
+Matrix4 MatIdentity() {
 	static const Matrix4 result{
 		1.0f,0.0f,0.0f,0.0f,
 		0.0f,1.0f,0.0f,0.0f,
@@ -28,7 +28,7 @@ Matrix4 MatScale(const Vector3& s) {
 }
 
 // 回転行列の設定
-Matrix4 MatRotationX(float angle){
+Matrix4 MatRotationX(float angle) {
 	float sin = std::sin(angle);
 	float cos = std::cos(angle);
 
@@ -42,7 +42,7 @@ Matrix4 MatRotationX(float angle){
 	return result;
 }
 
-Matrix4 MatRotationY(float angle){
+Matrix4 MatRotationY(float angle) {
 	float sin = std::sin(angle);
 	float cos = std::cos(angle);
 
@@ -56,7 +56,7 @@ Matrix4 MatRotationY(float angle){
 	return result;
 }
 
-Matrix4 MatRotationZ(float angle){
+Matrix4 MatRotationZ(float angle) {
 	float sin = std::sin(angle);
 	float cos = std::cos(angle);
 
@@ -71,7 +71,7 @@ Matrix4 MatRotationZ(float angle){
 }
 
 //平行移動行列の設定
-Matrix4 MatTranslation(const Vector3& t){
+Matrix4 MatTranslation(const Vector3& t) {
 	Matrix4 result{
 		1.0f,0.0f,0.0f,0.0f,
 		0.0f,1.0f,0.0f,0.0f,
@@ -83,60 +83,60 @@ Matrix4 MatTranslation(const Vector3& t){
 }
 
 // 座標変換 (ベクトルと行列の掛け算をする)
-Vector3 Vec3Transform(const Vector3& v, const Matrix4& m){
-	float v_[4] = {v.x,v.y,v.z,1};
+Vector3 Vec3Transform(const Vector3& v, const Matrix4& m) {
+	float v_[4] = { v.x,v.y,v.z,1 };
 
 	//v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + (1 * ) m.m[3][3];
 	float w = 0;
-		
-	for (int i = 0; i < 4; i++){
-		w += (v_[i] *  m.m[i][3]);
+
+	for (int i = 0; i < 4; i++) {
+		w += (v_[i] * m.m[i][3]);
 	}
 
 	Vector3 result = {};
-		/*{
-		(v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + m.m[3][0]) / w,
-		(v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + m.m[3][1]) / w,
-		(v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + m.m[3][2]) / w,
-	};*/
+	/*{
+	(v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + m.m[3][0]) / w,
+	(v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + m.m[3][1]) / w,
+	(v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + m.m[3][2]) / w,
+};*/
 
-	float ans[3] = {0,0,0};
+	float ans[3] = { 0,0,0 };
 
-	for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 4; j++){
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
 			ans[i] += v_[i] * m.m[j][i] / w;
 		}
 	}
 
-	result = { 
+	result = {
 		ans[0],
 		ans[1],
-		ans[2] 
+		ans[2]
 	};
 
 	return result;
 }
 
 // 代入演算子オーバーロード
-Matrix4& operator*=(Matrix4& m1, const Matrix4& m2){
-	Matrix4 result{0};
+Matrix4& operator*=(Matrix4& m1, const Matrix4& m2) {
+	Matrix4 result = { 0 };
 
-	for (int i = 0; i < 4; i++){
-		for (int j = 0; j < 4; j++){
-			for (int k = 0; k < 4; k++){
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
 				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
 			}
 		}
 	}
 
-	return result;
+	return m1 = result;
 }
 
-const Matrix4 operator*(const Matrix4& m1, const Matrix4& m2){
+const Matrix4 operator*(const Matrix4& m1, const Matrix4& m2) {
 	Matrix4 result = m1;
 	return result *= m2;
 }
 
-const Vector3 operator*(const Vector3& v, const Matrix4& m){
-	return Vec3Transform(v,m);
+const Vector3 operator*(const Vector3& v, const Matrix4& m) {
+	return Vec3Transform(v, m);
 }
