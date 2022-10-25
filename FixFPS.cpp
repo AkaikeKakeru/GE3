857c9a1705b91bc4b528df982358a97d834c57bb
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-25 09:59:50 +0900

    1フレーム分の時間を表す定数を定義

--- FixFPS.cpp
+++ FixFPS.cpp
@@ -6,4 +6,13 @@ void FixFPS::Initialize(){
 }
 
 void FixFPS::Update(){
+#pragma region 定数の定義
+	// 1/60秒ピッタリの時間
+	const std::chrono::microseconds
+		kMinTime(uint64_t(1000000.0f / 60.0f));
+
+	// 1/60秒よりわずかに短い時間
+	const std::chrono::microseconds
+		kMinCheckTime(uint64_t(1000000.0f / 65.0f));
+#pragma endregion
 }
