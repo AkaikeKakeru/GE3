383299b5184f876760d942d8c5f626d183be6350
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-26 01:52:39 +0900

    頂点データを記述

--- DrawBasis.cpp
+++ DrawBasis.cpp
@@ -1,7 +1,19 @@
 #include "DrawBasis.h"
 #include <DirectXMath.h>
+#include "Vector3.h"
+#include <wrl.h>
 
 using namespace DirectX;
 
 void DrawBasis::Initialize(){
+	//頂点データ
+	Vector3 vertices[] = {
+		//x		 y		z
+		{-5.0f, -5.0f, -5.0f},//左下
+		{-5.0f,  5.0f, -5.0f},//左上
+		{ 5.0f, -5.0f, -5.0f},//右下
+	};
+
+	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
+	UINT sizeVB = static_cast<UINT>(sizeof(Vector3) * _countof(vertices));
 }
