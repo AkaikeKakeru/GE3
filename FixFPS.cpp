a11a2bd36a27a90f4e502d2de8d1afdf13a0ea1d
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-25 10:23:09 +0900

    更新処理を書く

--- FixFPS.cpp
+++ FixFPS.cpp
@@ -1,7 +1,8 @@
 #include "FixFPS.h"
+#include <thread>
 
 void FixFPS::Initialize(){
-	//現在時間を記録
+	// 現在時間を記録
 	reference_ = std::chrono::steady_clock::now();
 }
 
@@ -15,4 +16,24 @@ void FixFPS::Update(){
 	const std::chrono::microseconds
 		kMinCheckTime(uint64_t(1000000.0f / 65.0f));
 #pragma endregion
+
+	// 現在時間取得
+	std::chrono::steady_clock::time_point now
+		= std::chrono::steady_clock::now();
+
+	// 前回記録からの経過時間取得
+	std::chrono::microseconds elapsed
+		= std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
+
+	// 1/60秒 (よりわずかに短い時間)立ってないなら
+	if (elapsed < kMinTime) {
+		// 1/60秒経過するまで微小なスリープを繰り返す
+		while (std::chrono::steady_clock::now() - reference_ < kMinTime) {
+			// 1マイクロ秒スリープ
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+	}
+
+	//現在の時間を記録
+	reference_ = std::chrono::steady_clock::now();
 }
