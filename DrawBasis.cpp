7a5afeb397fde4fa36a792b52886901412cbf4f9
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-26 10:53:26 +0900

    頂点バッファの確保と転送を記述

--- DrawBasis.cpp
+++ DrawBasis.cpp
@@ -16,4 +16,42 @@ void DrawBasis::Initialize(){
 
 	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
 	UINT sizeVB = static_cast<UINT>(sizeof(Vector3) * _countof(vertices));
+
+	//頂点バッファの設定
+	//ヒープ設定
+	D3D12_HEAP_PROPERTIES heapProp{};
+	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;//GPUの転送用
+										   //リソース設定
+	D3D12_RESOURCE_DESC resDesc{};
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Width = sizeVB;//頂点データ全体のサイズ
+	resDesc.Height = 1;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	//頂点バッファの生成
+	ComPtr<ID3D12Resource> vertBuff = nullptr;
+	result = dXBas->GetDevice()->CreateCommittedResource(
+		&heapProp,//ヒープ設定
+		D3D12_HEAP_FLAG_NONE,
+		&resDesc,//リソース設定
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&vertBuff));
+	assert(SUCCEEDED(result));
+
+	//GPU上のバッファに対応仮想メモリ(メインメモリ上)を取得
+	Vector3* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	assert(SUCCEEDED(result));
+
+	//全頂点に対して
+	for (int i = 0; i < _countof(vertices); i++) {
+		vertMap[i] = vertices[i];//座標をコピー
+	}
+
+	//繋がりを解除
+	vertBuff->Unmap(0, nullptr);
 }
