ddd9ebc79175768c23809668fe6d4254221313b1
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-26 11:28:21 +0900

    サンプルマスク、ラスタライザステート、ブレンドステートらの設定を記述

--- DrawBasis.cpp
+++ DrawBasis.cpp
@@ -170,4 +170,16 @@ void DrawBasis::CreateGraphicsPopeline(){
 	pipelineDesc.VS.BytecodeLength = vsBlob_->GetBufferSize();
 	pipelineDesc.PS.pShaderBytecode = psBlob_->GetBufferPointer();
 	pipelineDesc.PS.BytecodeLength = psBlob_->GetBufferSize();
+
+	//サンプルマスクの設定
+	pipelineDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;//標準設定
+
+	//ラスタライザの設定
+	pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;//カリングしない
+	pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;//ポリゴン内塗りつぶし
+	pipelineDesc.RasterizerState.DepthClipEnable = true;//深度クリッピングを有効に
+
+	//ブレンドステート
+	pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask
+		= D3D12_COLOR_WRITE_ENABLE_ALL;//RGB全てのチャネルを描画
 }
