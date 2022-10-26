a5c30c921806ef2d55ba49104a79eb0b7160f609
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-26 11:21:47 +0900

    頂点レイアウトを記述

--- DrawBasis.cpp
+++ DrawBasis.cpp
@@ -21,6 +21,20 @@ void DrawBasis::Initialize(DirectXBasis* dXBas){
 	LoadInstance(dXBas);
 
 	CompileShaderFile();
+
+	//頂点レイアウト
+	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
+		{
+			//xyz座標
+			"POSITION",									//セマンティック名
+			0,											//同じセマンティック名が複数あるときに使うインデックス(0でよい)
+			DXGI_FORMAT_R32G32B32_FLOAT,				//要素数とビット数を表す　(XYZの3つでfloat型なのでR32G32B32_FLOAT
+			0,											//入力スロットインデックス(0でよい)
+			D3D12_APPEND_ALIGNED_ELEMENT,				//データのオフセット値　(D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
+			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,	//入力データ種別　(標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DAT
+			0											//一度に描画するインスタンス数(0でよい)
+		},
+	};
 }
 
 void DrawBasis::LoadInstance(DirectXBasis* dXBas){
