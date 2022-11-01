a071f2e41a64458eae979e3dfdfc2f403644e25f
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-01 10:28:55 +0900

    DrawBasisで読み込むシェーダファイルは、Spriteシェーダの方にする

--- DrawBasis.cpp
+++ DrawBasis.cpp
@@ -144,7 +144,7 @@ void DrawBasis::CompileShaderFile(){
 
 	//頂点シェーダの読み込みとコンパイル
 	result = D3DCompileFromFile(
-		L"Resources/shaders/BasicVS.hlsl",//シェーダファイル名
+		L"Resources/shaders/SpriteVS.hlsl",//シェーダファイル名
 		nullptr,
 		D3D_COMPILE_STANDARD_FILE_INCLUDE,//インクルード可能にする
 		"main", "vs_5_0",//エントリーポイント名、シェーダ―モデル指定
@@ -169,7 +169,7 @@ void DrawBasis::CompileShaderFile(){
 
 	//ピクセルシェーダの読み込みとコンパイル
 	result = D3DCompileFromFile(
-		L"Resources/shaders/BasicPS.hlsl",//シェーダファイル名
+		L"Resources/shaders/SpritePS.hlsl",//シェーダファイル名
 		nullptr,
 		D3D_COMPILE_STANDARD_FILE_INCLUDE,//インクルード可能にする
 		"main", "ps_5_0",//エントリーポイント名、シェーダーモデル指定
