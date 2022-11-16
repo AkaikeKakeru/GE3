11632a40b2454f5e39844e5401f9ff1292c0472d
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-16 10:59:28 +0900

    ピクセルシェーダーにも、シェーダーヘッダーをインクルード

--- Resources/shaders/SpritePS.hlsl
+++ Resources/shaders/SpritePS.hlsl
@@ -1,7 +1,9 @@
-cbuffer ConstBufferDataMaterial:register(b0) {
-	float4 color;//êF(RGBA)
-};
+#include "SpriteBasic.hlsli"
+//
+//cbuffer ConstBufferDataMaterial:register(b0) {
+//	float4 color;//êF(RGBA)
+//};
 
-float4 main() : SV_TARGET{
-	return color;
+float4 main(VSOutput input) : SV_TARGET{
+	return float4(input.uv,0,1);
 }
