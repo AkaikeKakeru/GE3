8cc9cbfbd32ed02f7023eb97de377bbc0fe670d9
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-16 11:11:01 +0900

    頂点レイアウトの組み立て方を変更

--- DrawBasis.cpp
+++ DrawBasis.cpp
@@ -193,27 +193,38 @@ void DrawBasis::AssembleVertexLayout() {
 	}ElementName;
 
 	//頂点レイアウト
-	inputLayout_[ElementName::Position] = {
-		//xyz座標
-		"POSITION",									//セマンティック名
-		0,											//同じセマンティック名が複数あるときに使うインデックス(0でよい)
-		DXGI_FORMAT_R32G32B32_FLOAT,				//要素数とビット数を表す　(XYZの3つでfloat型なのでR32G32B32_FLOAT
-		0,											//入力スロットインデックス(0でよい)
-		D3D12_APPEND_ALIGNED_ELEMENT,				//データのオフセット値　(D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
-		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,	//入力データ種別　(標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DAT
-		0											//一度に描画するインスタンス数(0でよい)
-	};
+	for (int i = 0; i < ElementDescNum; i++){
+		switch (i){
+		case ElementName::Position:
+			inputLayout_[i] = {
+				//xyz座標
+				"POSITION",									//セマンティック名
+				0,											//同じセマンティック名が複数あるときに使うインデックス(0でよい)
+				DXGI_FORMAT_R32G32B32_FLOAT,				//要素数とビット数を表す　(XYZの3つでfloat型なのでR32G32B32_FLOAT
+				0,											//入力スロットインデックス(0でよい)
+				D3D12_APPEND_ALIGNED_ELEMENT,				//データのオフセット値　(D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
+				D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,	//入力データ種別　(標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DAT
+				0											//一度に描画するインスタンス数(0でよい)
+			};
+			break;
 
-	inputLayout_[ElementName::Texcoord] = {
-		//uv座標
-		"TEXCOORD",									//セマンティック名
-		0,											//同じセマンティック名が複数あるときに使うインデックス(0でよい)
-		DXGI_FORMAT_R32G32_FLOAT,				//要素数とビット数を表す　(XYZの3つでfloat型なのでR32G32B32_FLOAT
-		0,											//入力スロットインデックス(0でよい)
-		D3D12_APPEND_ALIGNED_ELEMENT,				//データのオフセット値　(D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
-		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,	//入力データ種別　(標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DAT
-		0											//一度に描画するインスタンス数(0でよい)
-	};
+		case ElementName::Texcoord:
+			inputLayout_[i] = {
+				//uv座標
+				"TEXCOORD",									//セマンティック名
+				0,											//同じセマンティック名が複数あるときに使うインデックス(0でよい)
+				DXGI_FORMAT_R32G32_FLOAT,				//要素数とビット数を表す　(XYZの3つでfloat型なのでR32G32B32_FLOAT
+				0,											//入力スロットインデックス(0でよい)
+				D3D12_APPEND_ALIGNED_ELEMENT,				//データのオフセット値　(D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
+				D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,	//入力データ種別　(標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DAT
+				0											//一度に描画するインスタンス数(0でよい)
+			};
+			break;
+
+		default:
+			break;
+		}
+	}
 }
 
 void DrawBasis::CreateGraphicsPipeline(DirectXBasis* dXBas) {
