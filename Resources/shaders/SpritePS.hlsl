ba5c6e50ebaf793ee9e2ffed8f74f3d65ef69bf4
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-17 12:08:03 +0900

    ピクセルシェーダにサンプラーとUV情報を渡し、テクスチャの色を取り出す

--- Resources/shaders/SpritePS.hlsl
+++ Resources/shaders/SpritePS.hlsl
@@ -1,9 +1,7 @@
 #include "SpriteBasic.hlsli"
-//
-//cbuffer ConstBufferDataMaterial:register(b0) {
-//	float4 color;//色(RGBA)
-//};
+Texture2D<float4> tex : register(t0); //0番スロットに設定されたテクスチャ
+SamplerState smp : register(s0); //0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET{
-	return float4(input.uv,0,1);
+	return float4(tex.Sample(smp,input.uv));
 }
