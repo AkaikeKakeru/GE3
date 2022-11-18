c610b4c1bb277b18c0cd384cb8fa2fa5e3995377
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-18 20:05:11 +0900

    別のブランチで作っていたものを丸コピ

---
+++ ViewProjection.cpp
@@ -0,0 +1,106 @@
+#include "ViewProjection.h"
+#include "WinApp.h"
+#include <DirectXMath.h>
+using namespace DirectX;
+
+void ViewProjection::Initialize() {
+	//今だけは致し方なく宣言(後々なくしたい)
+	XMMATRIX xmMatPro; //xmプロジェクション行列
+
+					   //初期化
+	cameraStatus_.eye_ = { 0, 0, -100 };	//視点座標
+	cameraStatus_.target_ = { 0, 0, 0 };	//注視点座標
+	cameraStatus_.up_ = { 0, 1, 0 };		//上方向ベクトル
+
+	scale_ = { 1,1,1 };
+	rotation_ = { 0, 0, 0 };
+	position_ = { 0,0,0 };
+
+	axisX_ = { 0,0,0 };
+	axisY_ = { 0,0,0 };
+	axisZ_ = { 0,0,0 };
+
+	cameraMoveVal_ = { 0,0,0 };
+
+#pragma region 投資投影変換行列の計算
+	xmMatPro =
+		XMMatrixPerspectiveFovLH(
+			XMConvertToRadians(45.0f),//上下画角45度
+			(float)WinApp::WinWidth / WinApp::WinHeight,//アスペクト比(画面横幅/画面縦幅)
+			0.1f, 1000.0f
+		);//前端、奥端
+
+#pragma region ビュー行列の作成
+	for (int i = 0; i < 4; i++) {
+		viewPro_.matPro_.m[i][0] = XMVectorGetX(xmMatPro.r[i]);
+		viewPro_.matPro_.m[i][1] = XMVectorGetY(xmMatPro.r[i]);
+		viewPro_.matPro_.m[i][2] = XMVectorGetZ(xmMatPro.r[i]);
+		viewPro_.matPro_.m[i][3] = XMVectorGetW(xmMatPro.r[i]);
+	}
+
+	CreateCameraCoordinateAxis(
+		cameraStatus_.eye_,
+		cameraStatus_.target_,
+		cameraStatus_.up_
+	);
+
+	CreateMatView();
+#pragma endregion
+}
+
+void ViewProjection::Update() {
+	CreateCameraCoordinateAxis(
+		cameraStatus_.eye_,
+		cameraStatus_.target_,
+		cameraStatus_.up_
+	);
+
+	CreateMatView();
+}
+
+void ViewProjection::CreateCameraCoordinateAxis(Vector3 eye, Vector3 target, Vector3 up) {
+	//視点→注視点へのベクトルをZ軸ベクトルとする。
+	axisZ_ = Vec3Normalize(target - eye);
+	//上方向ベクトルとZ軸ベクトルの外積をX軸ベクトルとする。
+	axisX_ = Vec3Normalize(Vec3Cross(up, axisZ_));
+	//Z軸ベクトルとX軸ベクトルの外積をY軸ベクトルとする。
+	axisY_ = Vec3Cross(axisZ_, axisX_);
+}
+
+void ViewProjection::UpdateMatWorld() {
+	Matrix4 matScale, matRota, matTrans, matX, matY, matZ;
+
+	//スケール、回転、平行移動行列の計算
+	matScale = Mat4Scale(scale_);
+	matRota = Mat4Identity();
+
+	matZ = Mat4RotationZ(rotation_.z);
+	matX = Mat4RotationX(rotation_.x);
+	matY = Mat4RotationY(rotation_.y);
+
+	matRota = matRota * matZ * matX * matY;
+
+	matTrans = Mat4Translation(position_);
+
+	//ワールド行列の合成
+	matWorld_ = Mat4Identity();	//変形リセット
+
+	matWorld_ *= matScale;	//ワールド行列のスケーリングを反映
+	matWorld_ *= matRota;	//ワールド行列に回転を反映
+	matWorld_ *= matTrans;	//ワールド行列に平行移動を反映
+}
+
+void ViewProjection::CreateMatView() {
+	cameraMoveVal_ = {
+		Vec3Dot(cameraStatus_.eye_,axisX_),
+		Vec3Dot(cameraStatus_.eye_,axisY_),
+		Vec3Dot(cameraStatus_.eye_,axisZ_)
+	};
+
+	viewPro_.matView_ = {
+		axisX_.x,axisX_.y,axisX_.z,0,
+		axisY_.x,axisY_.y,axisY_.z,0,
+		axisZ_.x,axisZ_.y,axisZ_.z,0,
+		-cameraMoveVal_.x,-cameraMoveVal_.y,-cameraMoveVal_.z,1
+	};
+}
--- ViewProjection.h
+++ ViewProjection.h
@@ -1,1 +1,71 @@
 #pragma once
+#include "Matrix4.h"
+#include "Vector3.h"
+
+class ViewProjection {
+public: //構造体
+		//定数バッファ用データ構造体(ビュープロジェクション)
+	typedef struct ConstBufferDataViewProjection {
+		Matrix4 matView_; //ビュー行列
+		Matrix4 matPro_; //プロジェクション行列
+	}ConstBufferDataViewProjection;
+
+	//カメラの状態
+	typedef struct CameraStatus {
+		Vector3 eye_;	//視点座標
+		Vector3 target_;	//注視点座標
+		Vector3 up_;		//上方向ベクトル
+	}CameraStatus;
+
+public: // 基本的なメンバ関数
+		//初期化
+	void Initialize();
+	//更新
+	void Update();
+
+private: //固有のメンバ関数
+		 //カメラ座標軸作成
+	void CreateCameraCoordinateAxis(Vector3 eye,Vector3 target,Vector3 up);
+	//ワールド座標行列の更新
+	void UpdateMatWorld();
+	//ビュー行列作成
+	void CreateMatView();
+
+public: //ゲッター
+		//ビュープロのゲッター
+	ConstBufferDataViewProjection GetViewProjection() const { return viewPro_; };
+
+public: //セッター
+		//視点セッター
+	void SetCameraEye(Vector3 eye) { cameraStatus_.eye_ = eye; };
+	//注視点セッター
+	void SetCameraTarget(Vector3 target) { cameraStatus_.target_ = target; };
+	//上方向セッター
+	void SetCameraUp(Vector3 up) { cameraStatus_.up_ = up; };
+
+private: //メンバ変数
+		 //ビュープロジェクション
+	ConstBufferDataViewProjection viewPro_ = {};
+	//カメラ状態
+	CameraStatus cameraStatus_ = {};
+
+	///アフィン変換情報
+	//スケール
+	Vector3 scale_ = {};
+	//回転
+	Vector3 rotation_ = {};
+	//位置
+	Vector3 position_ = {};
+
+	//ワールド変換行列
+	Matrix4 matWorld_ = {};
+
+	//カメラX軸
+	Vector3 axisX_ = {};
+	//カメラY軸
+	Vector3 axisY_ = {};
+	//カメラZ軸
+	Vector3 axisZ_ = {};
+	//カメラ移動値
+	Vector3 cameraMoveVal_ = {};
+};
