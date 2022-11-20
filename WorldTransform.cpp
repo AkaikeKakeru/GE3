63bf2c13ce16e0b25b6fb31e96b68c14ec5e208a
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-20 22:25:56 +0900

    とりあえずで仮に中身を書いてみる

--- WorldTransform.cpp
+++ WorldTransform.cpp
@@ -1,1 +1,40 @@
 #include "WorldTransform.h"
+
+void WorldTransform::Initialize(){
+	//アフィン変換情報
+	scale_ = { 1,1,1 };
+	rotation_ = { 0,0,0 };
+	position_ = { 0,0,0 };
+
+	matWorld_ = Mat4Identity();
+}
+
+void WorldTransform::RecalculationMatWorld(){
+	Matrix4 matScale, matRota, matTrans, matX, matY, matZ;
+
+	//スケール、回転、平行移動行列の計算
+	matScale = Mat4Scale(scale_);
+	matRota = Mat4Identity();
+
+	matZ = Mat4RotationZ(rotation_.z);
+	matX = Mat4RotationX(rotation_.x);
+	matY = Mat4RotationY(rotation_.y);
+
+	matRota = matRota * matZ * matX * matY;
+
+	matTrans = Mat4Translation(position_);
+
+	//ワールド行列の合成
+	matWorld_ = Mat4Identity();	//変形リセット
+
+	matWorld_ *= matScale;	//ワールド行列のスケーリングを反映
+	matWorld_ *= matRota;	//ワールド行列に回転を反映
+	matWorld_ *= matTrans;	//ワールド行列に平行移動を反映
+}
+
+void WorldTransform::TransferMatWorld(){
+	//定数バッファへデータ転送
+	constMapTransform_->mat = matWorld_ *
+		viewPro_->GetViewProjection().matView_*
+		viewPro_->GetViewProjection().matPro_;
+}
--- WorldTransform.h
+++ WorldTransform.h
@@ -1,8 +1,32 @@
 #pragma once
+#include "Vector3.h"
+#include "Matrix4.h"
+#include "ViewProjection.h"
 
 class WorldTransform{
 public:
+	//定数バッファ用データ構造体(3D変換行列)
+	struct ConstBufferDataTransform {
+		Matrix4 mat; //3D変換行列
+	};
 
+public:
+	void Initialize();
+	void RecalculationMatWorld();
+	void TransferMatWorld();
+		
 private:
-	
+	//アフィン変換情報
+	Vector3 scale_{};
+	Vector3 rotation_{};
+	Vector3 position_{};
+
+	//ワールド変換行列
+	Matrix4 matWorld_{};
+
+	//ビュープロジェクション
+	ViewProjection* viewPro_{};
+
+	//3D変換定数マップ
+	ConstBufferDataTransform* constMapTransform_ = nullptr;
 };
