fe0905512d71ad66cc856fe1d75763bdae81d65e
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-22 09:25:38 +0900

    とりあえず元々のオブジェクトの処理を丸コピ

---
+++ ObjectBasis.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "Vector4.h"
+#include "Matrix4.h"
+#include <d3d12.h>
+#include <DirectXTex.h>
+#include <wrl.h>
+
+#include "WorldTransform.h"
+#include "ViewProjection.h"
+
+using namespace DirectX;
+
+class ObjectBasis {
+private:
+	template <class Type>
+	using ComPtr = Microsoft::WRL::ComPtr<Type>;
+
+public: // 構造体
+		//定数バッファ用データ構造体(マテリアル)
+	struct ConstBufferDataMaterial {
+		Vector4 color; //色(RGBA)
+	};
+
+	//資料05-02で追加
+#pragma region 3D変換行列
+	//定数バッファ用データ構造体(3D変換行列)
+	struct ConstBufferDataTransform {
+		Matrix4 mat; //3D変換行列
+	};
+
+	struct TextureData {
+		TexMetadata metadata{};
+		ScratchImage scratchImg{};
+		ScratchImage mipChine{};
+
+		D3D12_HEAP_PROPERTIES textureHeapProp{};
+		D3D12_RESOURCE_DESC textureResourceDesc{};
+
+		//テクスチャバッファの生成
+		ComPtr<ID3D12Resource> texBuff = nullptr;
+	};
+
+public://メンバ関数
+	void Initialize( ObjectBasis* model, ID3D12Device* device);
+	void InitializeTexture(TextureData* textureData, const wchar_t* szFile);
+	void TransferTextureBuffer(TextureData* textureData, ID3D12Device* device);
+	void SetInitialize( ObjectBasis* model, ID3D12Device* device, int objectNum);
+	void Update( ObjectBasis* model);
+
+	void Draw( ObjectBasis* model, ID3D12GraphicsCommandList* commandList, D3D12_VERTEX_BUFFER_VIEW& vbView,
+		D3D12_INDEX_BUFFER_VIEW& ibView, UINT numIndices);
+
+	void copyInit();
+	void copyUp();
+	void copyDraw();
+
+private://メンバ変数
+
+		//定数バッファ(行列用)
+	ComPtr<ID3D12Resource> constBuffTransform_ = {};
+
+	//定数バッファマップ(行列用)
+	ConstBufferDataTransform* constMapTransform_ = {};
+
+	//定数バッファ(ビュープロジェクション)
+	ViewProjection* viewProjection_ = nullptr;
+
+	//ワールド変換
+	//アフィン変換情報
+	WorldTransform worldTransform_{};
+
+	//親オブジェクトへのポインタ
+	ObjectBasis* parent_ = nullptr;
+};
